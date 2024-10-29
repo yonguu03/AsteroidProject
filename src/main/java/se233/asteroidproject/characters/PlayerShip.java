d037a20bdb08c11276564999abb32ad2137dc8fd
yonguu03 <mooyonguu03@gmail.com>
2024-10-29 19:32:42 +0700

    update

--- src/main/java/se233/asteroidproject/characters/PlayerShip.java
+++ src/main/java/se233/asteroidproject/characters/PlayerShip.java
@@ -29,14 +29,14 @@ public class PlayerShip extends Characters {
         this.remainingLives = remainingLives;
     }
 
-    private int remainHyperspaceJump;
+
     private boolean isUncollisionable = false;
 
     //get the player image from resource directory
     public PlayerShip() {
         super(new ImageView(new Image(PlayerShip.class.getResource("/Player.png").toExternalForm())), 380, 270);
         this.remainingLives = MAX_LIVES;
-        this.remainHyperspaceJump = 3;
+
     }
 
     @Override
@@ -73,33 +73,7 @@ public class PlayerShip extends Characters {
         this.movement = this.movement.add(changeX, changeY);
     }
 
-    public void hyperspaceJump(List<Characters> enemies) {
-        if (this.remainHyperspaceJump > 0) {
-            boolean collided;
-            do {
-                setNewLocation();
-                collided = false;
-                for (Characters enemy : enemies) {
-                    if (enemy.hasCollided(this)) {
-                        collided = true;
-                        break;
-                    }
-                }
-            } while (collided);
 
-            invisibility(Duration.seconds(3), Color.WHITE);
-            this.remainHyperspaceJump -= 1;
-        }
-    }
-
-    public void setNewLocation() {
-        Random r = new Random();
-        int hyperSpaceX = r.nextInt(AsteroidsGame.WIDTH - 50) + 50;
-        int hyperSpaceY = r.nextInt(AsteroidsGame.HEIGHT - 50) + 50;
-
-        this.entityShape.setTranslateX(hyperSpaceX);
-        this.entityShape.setTranslateY(hyperSpaceY);
-    }
 
     public void decreaseLife() {
         if (!isUncollisionable) {
@@ -110,8 +84,6 @@ public class PlayerShip extends Characters {
             pause.setOnFinished(event -> {
                 // back to og image
                 this.entityShape.setImage(new Image(PlayerShip.class.getResource("/Player.png").toExternalForm()));
-                entityShape.setTranslateX(AsteroidsGame.WIDTH / 2);
-                entityShape.setTranslateY(AsteroidsGame.HEIGHT / 2);
                 invisibility(Duration.seconds(1), Color.WHITE);
             });
             pause.play();
@@ -135,17 +107,17 @@ public class PlayerShip extends Characters {
     }
 
     public void dyingAnimation() {
-        // Change the spaceship image to PlayerDeath.png
+        // change the spaceship image to PlayerDeath.png
         this.entityShape.setImage(new Image(PlayerShip.class.getResource("/PlayerDeath.png").toExternalForm()));
 
-        // Put the ship in the center when player dies
+        // put the ship in the center when player dies
         double centerX = AsteroidsGame.WIDTH / 2;
         double centerY = AsteroidsGame.HEIGHT / 2;
 
         this.entityShape.setTranslateX(centerX);
         this.entityShape.setTranslateY(centerY);
 
-        // Debug statement to verify the method is called
+        // debug statement to verify the method is called
         System.out.println("Dying animation played, spaceship repositioned to center: " + centerX + ", " + centerY);
     }
 
